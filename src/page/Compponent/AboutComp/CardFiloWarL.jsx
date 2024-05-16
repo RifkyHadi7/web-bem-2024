c154b54649f463ffb4eee72f32f659c370ceda79
RifkyHadi7 <rifkyhadi7@gmail.com>
2024-05-16 21:33:45 +0700

    revisi filosifi warna

--- src/page/Compponent/AboutComp/CardFiloWarL.jsx
+++ src/page/Compponent/AboutComp/CardFiloWarL.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const CardFiloWarL = (props) => {
   return (
     <div className='flex flex-row max-w-96 max-h-64 lg:max-w-[610px] lg:max-h-[192px] rounded-[36px] lg:rounded-[50px] shadow-lg bg-cust-background items-center p-4'>
-        <div className='text-right font-lato font-semibold text-sm lg:text-xl'>
+        <div className='text-right font-lato font-medium text-sm lg:text-xl'>
             {props.desc}
         </div>
         <img src={props.srcimg} alt={props.alt} className='ml-1 max-w-28 lg:max-w-[200px]'/>
--- src/page/Compponent/AboutComp/CardFiloWarR.jsx
+++ src/page/Compponent/AboutComp/CardFiloWarR.jsx
@@ -4,7 +4,7 @@ const CardFiloWarR = (props) => {
   return (
     <div className='flex flex-row max-w-96 max-h-64 lg:max-w-[610px] lg:max-h-[192px] rounded-[36px] lg:rounded-[50px] shadow-lg bg-cust-background items-center p-4'>
         <img src={props.srcimg} alt={props.alt} className='mr-1 max-w-28 lg:max-w-[200px]'/>
-        <div className='text-left font-lato font-semibold text-sm lg:text-xl'>
+        <div className='text-left font-lato font-medium text-sm lg:text-xl'>
             {props.desc}
         </div>
     </div>
